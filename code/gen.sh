88d7efa545dd4f124de49d11d242bda2540cfd4f
survivi <ptyzchenjie@ruc.edu.cn>
2023-11-14 20:14:53 +0800

    update code

---
+++ code/gen.sh
@@ -0,0 +1,7 @@
+for model in alpaca-7b vicuna-7b vicuna-13b
+do
+    for file in Bio-Medical Finance Science Education Open-Domain
+        do
+            nohup python -u rlhf_generate.py --model $model --file $file >> output.log 2>&1 &
+        done
+done
