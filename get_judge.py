4808a59e1b52c7457def8a85ef99092965879059
survivi <ptyzchenjie@ruc.edu.cn>
2023-11-14 21:47:36 +0800

    update code and data

--- get_judge.py
+++ get_judge.py
@@ -7,36 +7,41 @@ save_path = "./all_data/update_add_judge/"
 for path in os.listdir(data_path):
     # if path != "chatgpt_judge":
     #     continue
+    for model in [
+        "chatgpt",
+        "text-davinci-002",
+        "text-davinci-003",
+        "llama-2-7b-chat-hf",
+        "llama-2-13b-chat-hf",
+        "alpaca-7b",
+        "vicuna-7b",
+        "vicuna-13b",
+        "llama-7b",
+        "claude-1",
+        "claude-2",
+        "llama-2-7b-hf",
+        "llama-2-13b-hf",
+        "bloom-7b1",
+    ]:
+        if path.startswith(model):
+            model_name = model
+            break
     check_exist(os.path.join(save_path, path))
     for i in os.listdir(os.path.join(data_path, path)):
         p = os.path.join(data_path, path, i)
         with open(p, "r", encoding="utf-8") as f:
             data = json.load(f)
             for index in range(len(data)):
-                for model in [
-                    "chatgpt",
-                    "text-davinci-002",
-                    "text-davinci-003",
-                    "llama-2-7b-chat-hf",
-                    "llama-2-13b-chat-hf",
-                    "alpaca-7b",
-                    "vicuna-7b",
-                    "vicuna-13b",
-                    "llama-7b",
-                    "claude-1",
-                    "claude-2",
-                    "llama-2-7b-hf",
-                    "llama-2-13b-hf",
-                    "bloom-7b1",
-                ]:
-                    if path.startswith(model):
-                        model_name = model
-                        break
                 facts_lst = data[index][model_name + "_fact"]
                 if len(facts_lst) == 0:
                     data[index][model_name + "_judge"] = []
                     continue
                 ans = data[index][model_name + "_judge"]
+                if ans == "NO FACTS":
+                    data[index][model_name + "_judge"] = [
+                        f"unknown" for _ in range(len(facts_lst))
+                    ]
+                    continue
                 lines = [line.strip() for line in ans.split("\n") if line]
                 if len(lines) != len(facts_lst):
                     print("File: " + p)
