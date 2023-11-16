ac20815a5c216cfff5f0b8c4c609bc4aa73b3bfa
survivi <ptyzchenjie@ruc.edu.cn>
2023-11-16 16:22:22 +0800

    update code and data

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 export CUDA_VISIBLE_DEVICES="8"
-python test.py --all-files --model bloom-7b1 --data-dir "./data/" --save-dir "./temp/"
+python test.py --all-files --model llama-2-13b-hf --data-dir "./data/" --save-dir "./temp/"
