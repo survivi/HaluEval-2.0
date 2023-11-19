750cf3b305d86b6e3e1cfd3e93b3eb6696352992
survivi <ptyzchenjie@ruc.edu.cn>
2023-11-19 14:37:00 +0800

    update code and judge

---
+++ fact_judge/fact.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ModelList=("chatgpt" "text-davinci-002" "text-davinci-003" "llama-2-7b-chat-hf" "llama-2-13b-chat-hf" "alpaca-7b" "vicuna-7b" "vicuna-13b")
+for model in ${ModelList[*]}; do
+    nohup python -u fact.py --all-files --model $model >> ./fact_$model.log 2>&1 &
+done
--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 export CUDA_VISIBLE_DEVICES="8,9"
 python test.py --all-files --model llama-2-13b-hf --data-dir "./data/" --save-dir "./temp/"
