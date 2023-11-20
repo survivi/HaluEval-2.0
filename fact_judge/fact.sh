484b01ad684963f278ff3e3590c684c7f4a66be7
survivi <ptyzchenjie@ruc.edu.cn>
2023-11-21 00:10:42 +0800

    update code and data

--- fact_judge/fact.sh
+++ fact_judge/fact.sh
@@ -2,5 +2,5 @@
 
 ModelList=("chatgpt" "text-davinci-002" "text-davinci-003" "llama-2-7b-chat-hf" "llama-2-13b-chat-hf" "alpaca-7b" "vicuna-7b" "vicuna-13b")
 for model in ${ModelList[*]}; do
-    nohup python -u fact.py --all-files --model $model >> ./fact_$model.log 2>&1 &
+    nohup python -u fact.py --all-files --model $model >> ./log/fact_$model.log 2>&1 &
 done
--- fact_judge/judge.sh
+++ fact_judge/judge.sh
@@ -2,5 +2,5 @@
 
 ModelList=("chatgpt" "text-davinci-002" "text-davinci-003" "llama-2-7b-chat-hf" "llama-2-13b-chat-hf" "alpaca-7b" "vicuna-7b" "vicuna-13b")
 for model in ${ModelList[*]}; do
-    nohup python -u judge.py --all-files --model $model >> ./judge_$model.log 2>&1 &
+    nohup python -u judge.py --all-files --model $model >> ./log/judge_$model.log 2>&1 &
 done
