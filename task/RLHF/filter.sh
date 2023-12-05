28af7c199130eda2f6de6901ca73e7a8d448e8b8
survivi <ptyzchenjie@ruc.edu.cn>
2023-12-05 17:31:20 +0800

    update code and data

--- task/RLHF/filter.sh
+++ task/RLHF/filter.sh
@@ -2,5 +2,5 @@
 
 for model in alpaca-7b vicuna-7b vicuna-13b
 do
-    nohup python -u rlhf_filter.py --model $model --all-files >> ./log/filter_$model\_$file.log 2>&1 &
+    nohup python -u rlhf_filter.py --model $model --all-files >> ./log/filter_$model.log 2>&1 &
 done
--- task/RLHF/generate.sh
+++ task/RLHF/generate.sh
@@ -2,8 +2,5 @@
 
 for model in alpaca-7b vicuna-7b vicuna-13b
 do
-    for file in Bio-Medical Finance Science Education Open-Domain
-    do
-        nohup python -u rlhf_generate.py --model $model --all-files >> ./log/gen_$model\_$file.log 2>&1 &
-    done
+    nohup python -u rlhf_generate.py --model $model --all-files >> ./log/gen_$model.log 2>&1 &
 done
