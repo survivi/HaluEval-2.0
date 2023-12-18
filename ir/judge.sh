0698caad6c99357bed96b24570496f699cbe5ef3
survivi <ptyzchenjie@ruc.edu.cn>
2023-12-18 15:32:22 +0800

    update code and data

--- ir/judge.sh
+++ ir/judge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export OPENAI_API_KEY="sk-vu2yPgLCrnFqq9AT823cEbF63d1249168bBd64A54a39A818"
+export OPENAI_API_KEY="sk-uQgp6DoH8VpwMphgC44e7f04DdA24f37B5A6C2B899A9F040"
 export OPENAI_API_BASE="https://api.chatgpt-3.vip/v1"
 echo "OPENAI_API_KEY is set to: $OPENAI_API_KEY"
 echo "OPENAI_API_BASE is set to: $OPENAI_API_BASE"
@@ -10,6 +10,6 @@ DirList=("response-10")
 ModelList=("chatgpt" "llama-2-7b-chat-hf")
 for dir in ${DirList[*]}; do
     for model in ${ModelList[*]}; do
-        nohup python -u judge.py --all-files --model $model --data-dir ./fact/$dir/$model --save-dir ./judge/$dir/$model >> ./log/judge_$dir\_$model.log 2>&1 &
+        nohup python -u judge.py --all-files --model $model --data-dir ./fact/$dir/$model --save-dir ./ir_response_judge/$dir/$model >> ./log/judge_$dir\_$model.log 2>&1 &
     done
 done
--- sft_judge.sh
+++ sft_judge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export OPENAI_API_KEY="sk-vu2yPgLCrnFqq9AT823cEbF63d1249168bBd64A54a39A818"
+export OPENAI_API_KEY="sk-uQgp6DoH8VpwMphgC44e7f04DdA24f37B5A6C2B899A9F040"
 export OPENAI_API_BASE="https://api.chatgpt-3.vip/v1"
 echo "OPENAI_API_KEY is set to: $OPENAI_API_KEY"
 echo "OPENAI_API_BASE is set to: $OPENAI_API_BASE"
