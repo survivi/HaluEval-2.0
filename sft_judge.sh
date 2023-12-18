0afd99eae2fd7b5c8e025e74557d1b4174e88edf
survivi <ptyzchenjie@ruc.edu.cn>
2023-12-18 11:09:46 +0800

    update code and data

--- sft_judge.sh
+++ sft_judge.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export OPENAI_API_KEY="sk-vu2yPgLCrnFqq9AT823cEbF63d1249168bBd64A54a39A818"
+export OPENAI_API_BASE="https://api.chatgpt-3.vip/v1"
+echo "OPENAI_API_KEY is set to: $OPENAI_API_KEY"
+echo "OPENAI_API_BASE is set to: $OPENAI_API_BASE"
+
 DirList=("flan" "sharegpt" "sinstruct" "sinstruct_sharegpt" "sinstruct_sharegpt_flan_10k")
 
 for dir in ${DirList[*]}; do
