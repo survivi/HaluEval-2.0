0b6faf3bb42ceb44378a3e841e7c5e9744bc932f
survivi <ptyzchenjie@ruc.edu.cn>
2023-12-18 11:28:09 +0800

    update code and data

--- ir/fact.sh
+++ ir/fact.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export OPENAI_API_KEY="sk-oLf6htFGHwu9Hc1ABb5eCe82E7C34b9e84A9D9Ef1bF472E5"
-export OPENAI_API_BASE="https://api.pumpkinaigc.online/v1"
+export OPENAI_API_KEY="sk-vu2yPgLCrnFqq9AT823cEbF63d1249168bBd64A54a39A818"
+export OPENAI_API_BASE="https://api.chatgpt-3.vip/v1"
 echo "OPENAI_API_KEY is set to: $OPENAI_API_KEY"
 echo "OPENAI_API_BASE is set to: $OPENAI_API_BASE"
 
-DirList=("response-1" "response-50" "response-100")
-# ModelList=("chatgpt" "text-davinci-002" "text-davinci-003" "llama-2-7b-chat-hf" "llama-2-13b-chat-hf" "alpaca-7b" "vicuna-7b" "vicuna-13b" "claude-1" "claude-2")
+# DirList=("response-1" "response-50" "response-100")
+DirList=("response-10")
 ModelList=("chatgpt" "llama-2-7b-chat-hf")
 for dir in ${DirList[*]}; do
     for model in ${ModelList[*]}; do
--- ir/judge.sh
+++ ir/judge.sh
@@ -5,8 +5,8 @@ export OPENAI_API_BASE="https://api.chatgpt-3.vip/v1"
 echo "OPENAI_API_KEY is set to: $OPENAI_API_KEY"
 echo "OPENAI_API_BASE is set to: $OPENAI_API_BASE"
 
-DirList=("response-1" "response-50" "response-100")
-# ModelList=("chatgpt" "text-davinci-002" "text-davinci-003" "llama-2-7b-chat-hf" "llama-2-13b-chat-hf" "alpaca-7b" "vicuna-7b" "vicuna-13b" "claude-1" "claude-2")
+# DirList=("response-1" "response-50" "response-100")
+DirList=("response-10")
 ModelList=("chatgpt" "llama-2-7b-chat-hf")
 for dir in ${DirList[*]}; do
     for model in ${ModelList[*]}; do
