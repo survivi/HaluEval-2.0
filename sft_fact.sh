1a51694380c66a60ea021661d10b92689df02d9a
survivi <ptyzchenjie@ruc.edu.cn>
2023-12-18 11:08:14 +0800

    update code

--- sft_fact.sh
+++ sft_fact.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DirList=("complexity" "difficulty" "diversity" "scaling")
+DirList=("flan" "sharegpt" "sinstruct" "sinstruct_sharegpt" "sinstruct_sharegpt_flan_10k")
 
 for dir in ${DirList[*]}; do
-    nohup python -u fact.py --all-files --model llama-7b --data-dir "./sft_data/$dir" --save-dir "./fact/$dir" >> ./log/sft_fact_$dir.log 2>&1 &
+    nohup python -u fact.py --all-files --model llama-7b --data-dir "./sft2/$dir" --save-dir "./fact/$dir" >> ./log/sft_fact_$dir.log 2>&1 &
 done
--- sft_judge.sh
+++ sft_judge.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DirList=("complexity" "difficulty" "diversity" "scaling")
+DirList=("flan" "sharegpt" "sinstruct" "sinstruct_sharegpt" "sinstruct_sharegpt_flan_10k")
 
 for dir in ${DirList[*]}; do
     nohup python -u judge.py --all-files --model llama-7b --data-dir "./fact/$dir" --save-dir "./judge/$dir" >> ./log/sft_judge_$dir.log 2>&1 &
