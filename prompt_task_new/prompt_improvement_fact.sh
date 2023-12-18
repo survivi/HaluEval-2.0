d84ce966c386a612ee11c79fa9579c39ca20f2b1
survivi <ptyzchenjie@ruc.edu.cn>
2023-12-18 21:21:40 +0800

    update code

--- prompt_task_new/prompt_improvement_fact.sh
+++ prompt_task_new/prompt_improvement_fact.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export OPENAI_API_KEY="sk-uQgp6DoH8VpwMphgC44e7f04DdA24f37B5A6C2B899A9F040"
+export OPENAI_API_BASE="https://api.chatgpt-3.vip/v1"
+echo "OPENAI_API_KEY is set to: $OPENAI_API_KEY"
+echo "OPENAI_API_BASE is set to: $OPENAI_API_BASE"
+
 ModelList=("llama-2-7b-chat-hf")
 DirList=("0-shot-cot" "few-shot-cot" "refine-q" "human_detailed" "model_detailed")
 for model in ${ModelList[*]}; do
