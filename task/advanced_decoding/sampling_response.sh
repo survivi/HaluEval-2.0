e594863d46124ad7b1a956ea471a9da3e56dd15b
survivi <ptyzchenjie@ruc.edu.cn>
2023-12-27 10:58:40 +0800

    update

--- task/advanced_decoding/sampling_response.sh
+++ task/advanced_decoding/sampling_response.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# greedy-nucleus sampling
 nohup python -u response.py --all-files --model llama-2-7b-chat-hf --do-sample --top-k 0 --top-p 0.5 --data-dir ./annotation/query/ --save-dir ./gns_response/ > gns.log 2>&1 &
 
+# factual-nucleus sampling
 nohup python -u response.py --all-files --model llama-2-7b-chat-hf --do-sample --top-k 0 --top-p 0.9 --data-dir ./annotation/query/ --save-dir ./fns_response/ > fns.log 2>&1 &
