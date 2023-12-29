fe5ee843482dc8014583ca78ec0d6451c4403b2c
survivi <ptyzchenjie@ruc.edu.cn>
2023-12-29 14:53:53 +0800

    update

--- alpaca-new/fact.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ModelList=("alpaca-7b")
-for model in ${ModelList[*]}; do
-    nohup python -u fact.py --all-files --model $model >> ./log/fact_$model.log 2>&1 &
-done
--- alpaca-new/judge.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ModelList=("alpaca-7b")
-for model in ${ModelList[*]}; do
-    nohup python -u judge.py --all-files --model $model >> ./log/judge_$model.log 2>&1 &
-done
