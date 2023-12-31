13ff54217aaec205bfbe7faab51741400914caad
survivi <ptyzchenjie@ruc.edu.cn>
2023-12-31 12:00:12 +0800

    update

--- prompt_new/fact.sh
+++ prompt_new/fact.sh
@@ -6,7 +6,7 @@
 # echo "OPENAI_API_BASE is set to: $OPENAI_API_BASE"
 
 ModelList=("chatgpt" "llama-2-7b-chat-hf")
-DirList=("0-shot" "base" "character_info" "domain" "few-shot" "in-context_demo" "mannual_desc" "refine-q" "retrieved_demo" "reverse_pos" "synthetic_demo" "synthetic_desc" "wrong_demo")
+DirList=("0-shot" "base" "character_info" "domain_info" "few-shot" "in-context_demo" "mannual_desc" "refine-q" "retrieved_demo" "reverse_pos" "synthetic_demo" "synthetic_desc" "wrong_demo")
 for model in ${ModelList[*]}; do
     for dir in ${DirList[*]}; do
         nohup python -u fact.py --all-files --model $model --data-dir "./prompt_response_pure/$model/$dir" --save-dir "./fact/$model/$dir/" >> ./fact_log/fact_$dir\_$model.log 2>&1 &
--- prompt_new/judge.sh
+++ prompt_new/judge.sh
@@ -6,7 +6,7 @@
 # echo "OPENAI_API_BASE is set to: $OPENAI_API_BASE"
 
 ModelList=("chatgpt" "llama-2-7b-chat-hf")
-DirList=("0-shot" "base" "character_info" "domain" "few-shot" "in-context_demo" "mannual_desc" "refine-q" "retrieved_demo" "reverse_pos" "synthetic_demo" "synthetic_desc" "wrong_demo")
+DirList=("0-shot" "base" "character_info" "domain_info" "few-shot" "in-context_demo" "mannual_desc" "refine-q" "retrieved_demo" "reverse_pos" "synthetic_demo" "synthetic_desc" "wrong_demo")
 for model in ${ModelList[*]}; do
     for dir in ${DirList[*]}; do
         nohup python -u judge.py --all-files --model $model --data-dir "./fact/$model/$dir" --save-dir "./prompt_judge/$model/$dir" >> ./judge_log/judge_$dir\_$model.log 2>&1 &
