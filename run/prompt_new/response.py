a23ec2c05932a8231a707ce2a5b1fdc954ee54c8
survivi <ptyzchenjie@ruc.edu.cn>
2024-01-01 10:38:54 +0800

    update

--- run/prompt_new/response.py
+++ run/prompt_new/response.py
@@ -119,7 +119,7 @@ class Chatbot(Bot):
         self.data_path = data_path  # path to data
         self.save_path = save_path  # path to save
         self.save_data = []  # data to save
-        self.max_retry = 8  # max retry times
+        self.max_retry = 3  # max retry times
         self.frequency = 2  # save frequency
 
     def load_data(self, part=0):
