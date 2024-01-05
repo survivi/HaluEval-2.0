02853dc77aca5e30acefcdcc686c27d6ebe73fa0
survivi <ptyzchenjie@ruc.edu.cn>
2024-01-05 23:54:28 +0800

    update

--- task/SFT_old/llama-7b_fact.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export OPENAI_API_KEY="sk-"
-export OPENAI_API_BASE="https://api.openai.com/v1"
-echo "OPENAI_API_KEY is set to: $OPENAI_API_KEY"
-echo "OPENAI_API_BASE is set to: $OPENAI_API_BASE"
-
-nohup python -u fact.py --all-files --model llama-7b --data-dir "./llama-7b/" --save-dir "./fact/llama-7b/" >> ./log/fact_llama-7b.log 2>&1 &
--- task/SFT_old/llama-7b_response.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export OPENAI_API_KEY="sk-"
-export OPENAI_API_BASE="https://api.openai.com/v1"
-echo "OPENAI_API_KEY is set to: $OPENAI_API_KEY"
-echo "OPENAI_API_BASE is set to: $OPENAI_API_BASE"
-
-nohup python -u response.py --all-files --model llama-7b --data-dir ./annotation/query/ --save-dir ./llama-7b/ >> ./log/llama-7b.log 2>&1 &
